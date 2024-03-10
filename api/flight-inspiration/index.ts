35ec688712dcc252766f2e5ab158413f9abec65b
zoheb598 <x22447782@student.ncirl.ie>
2024-03-10 19:02:45 +0000

    [refac]

--- api/flight-inspiration/index.ts
+++ api/flight-inspiration/index.ts
@@ -1,7 +1,6 @@
 import { VercelRequest, VercelResponse } from '@vercel/node';
 import getToken from '../helpers/getAuthToken.js';
 import getFlightsInspo from './getFlightsInspo.js';
-import strictParseInt from '../utils/strictParseInt.js';
 
 export default async (req: VercelRequest, res: VercelResponse) => {
     let token = await getToken();
@@ -13,9 +12,9 @@ export default async (req: VercelRequest, res: VercelResponse) => {
             // const { departureDate, origin, maxPrice } = { departureDate: '2024-03-20', origin: 'MAD', maxPrice: '200' }
             const { departureDate, origin, maxPrice } = req.query
             let clientReq = {
-                departureDate: departureDate,
-                origin: origin,
-                maxPrice: [Array.isArray(maxPrice) ? strictParseInt(maxPrice[0]) : strictParseInt(maxPrice)]
+                departureDate,
+                origin,
+                maxPrice
             }
 
             getFlightsInspo(token, clientReq)
