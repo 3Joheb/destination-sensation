bad9b57e10831a553201b9530c12f122c3435d01
Joheb133 <x22447782@student.ncirl.ie>
2024-03-20 17:02:12 +0000

    [chore] add react router

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,23 @@ import winterImg from "./assets/winter.jpg"
 import Navbar from "./components/Navbar"
 
 const App = () => {
+
     return (
         <div>
-            <Navbar/>
+            <Navbar />
 
             <div>WELCOME</div>
 
             <p> Hi there us fellow members of DESTINATION SENSATION are bringing this new holiday package website to you! We aim to help
                 making your holiday descision easier by making the biggest factor in your holiday way easier to chose , you
                 give us a price and time we will find you the places best suited to your budget!
-
             </p>
-            <img src={beachImg} alt="" />
-            <img src={cabinImg} alt="" />
-            <img src={winterImg} alt="" />
+            <div className="images">
+                <img src={beachImg} alt="" />
+                <img src={cabinImg} alt="" />
+                <img src={winterImg} alt="" />
+            </div>
+
             <p></p>
 
 
