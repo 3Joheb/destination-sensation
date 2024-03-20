8b3ad4f59fd1438b0399e0ed971b25b33034ff82
Joheb133 <x22447782@student.ncirl.ie>
2024-03-20 16:38:17 +0000

    [chore] make navbar into component

--- src/App.tsx
+++ src/App.tsx
@@ -2,36 +2,24 @@ import "./index.css"
 import beachImg from "./assets/beach.jpg"
 import cabinImg from "./assets/cabin.jpg"
 import winterImg from "./assets/winter.jpg"
-import navbar from "./navbar/navbar.tsx"
+import Navbar from "./components/Navbar"
 
-const App = () =>{
+const App = () => {
     return (
         <div>
-             <nav>
-        <ul>
-          <li>
-            <a href="/">Sign Up</a>
-          </li>
-          <li>
-            <a href="/home">Home</a>
-          </li>
-          <li>
-            <a href="/build">Holiday Builder</a>
-          </li>
-        </ul>
-      </nav>
+            <Navbar/>
 
-        <div>WELCOME</div>
+            <div>WELCOME</div>
 
-<p> Hi there us fellow members of DESTINATION SENSATION are bringing this new holiday package website to you! We aim to help
-    making your holiday descision easier by making the biggest factor in your holiday way easier to chose , you 
-    give us a price and time we will find you the places best suited to your budget!
+            <p> Hi there us fellow members of DESTINATION SENSATION are bringing this new holiday package website to you! We aim to help
+                making your holiday descision easier by making the biggest factor in your holiday way easier to chose , you
+                give us a price and time we will find you the places best suited to your budget!
 
-</p>
-<img src={beachImg} alt="" />
-<img src={cabinImg} alt=""/>
-<img src={winterImg} alt=""/>
-<p></p>
+            </p>
+            <img src={beachImg} alt="" />
+            <img src={cabinImg} alt="" />
+            <img src={winterImg} alt="" />
+            <p></p>
 
 
         </div>
