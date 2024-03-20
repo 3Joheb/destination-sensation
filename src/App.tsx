2200601c734b16228af0414e355a471bd5a1052d
AdrianaFTA <116352942+AdrianaFTA@users.noreply.github.com>
2024-03-20 16:16:24 +0000

    added images home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,28 @@
 import "./index.css"
+import beachImg from "./assets/beach.jpg"
+import cabinImg from "./assets/cabin.jpg"
+import winterImg from "./assets/winter.jpg"
+import navbar from "./navbar"
 
 const App = () =>{
     return (
-        <div>Hello, World</div>
+        <div>
+            
+        <div>WELCOME</div>
+
+<p> Hi there us fellow members of DESTINATION SENSATION are bringing this new holiday package website to you! We aim to help
+    making your holiday descision easier by making the biggest factor in your holiday way easier to chose , you 
+    give us a price and time we will find you the places best suited to your budget!
+
+</p>
+<img src={beachImg} alt="" />
+<img src={cabinImg} alt=""/>
+<img src={winterImg} alt=""/>
+<p></p>
+
+
+        </div>
+
     )
 }
 
