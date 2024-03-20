223776df956632d6d34d98b4f56863de4f894859
AdrianaFTA <116352942+AdrianaFTA@users.noreply.github.com>
2024-03-20 16:29:29 +0000

    navbar

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,25 @@ import "./index.css"
 import beachImg from "./assets/beach.jpg"
 import cabinImg from "./assets/cabin.jpg"
 import winterImg from "./assets/winter.jpg"
-import navbar from "./navbar"
+import navbar from "./navbar/navbar.tsx"
 
 const App = () =>{
     return (
         <div>
-            
+             <nav>
+        <ul>
+          <li>
+            <a href="/">Sign Up</a>
+          </li>
+          <li>
+            <a href="/home">Home</a>
+          </li>
+          <li>
+            <a href="/build">Holiday Builder</a>
+          </li>
+        </ul>
+      </nav>
+
         <div>WELCOME</div>
 
 <p> Hi there us fellow members of DESTINATION SENSATION are bringing this new holiday package website to you! We aim to help
---
+++ src/navbar/navbar.tsx
@@ -0,0 +1,19 @@
+const navbar=() =>{
+    return(
+        <nav>
+        <ul>
+          <li>
+            <a href="/">Sign Up</a>
+          </li>
+          <li>
+            <a href="/home">Home</a>
+          </li>
+          <li>
+            <a href="/build">Holiday Builder</a>
+          </li>
+        </ul>
+      </nav>
+
+    )
+}
+export default navbar
