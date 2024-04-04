a8b42409fe74fe018dc1ebff77b203b0983c1a1c
kaitlinmoore21 <117903693+kaitlinmoore21@users.noreply.github.com>
2024-04-04 15:38:15 +0100

    Created Card

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,15 @@
+
+const Card = () => {
+    return (
+        <div>
+                <h1>Destination </h1>
+                <h1>Price</h1>
+            <ul>
+                <li>Departure date</li>
+                <li>Return date</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Card;
