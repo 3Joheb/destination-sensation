2a42a3e197406783d4175cc5f12a3772fce22c17
zoheb598 <x22447782@student.ncirl.ie>
2024-04-09 17:08:42 +0100

    country(): [feat] basic error handling

--- api/flight-inspiration/getCountryName.ts
+++ api/flight-inspiration/getCountryName.ts
@@ -1,6 +1,4 @@
 
-
-
 const getAirportCountryCode = async (IATA: string) => {
     const key = process.env.IATA_API_KEY
 
@@ -13,22 +11,31 @@ const getAirportCountryCode = async (IATA: string) => {
         }
     };
 
+    // Initialize error object
+    let errorObj: { error?: {}, code?: number, message?: string, data?: {} } = {};
+
     try {
         const response = await fetch(url, options);
+        if (!response.ok) {
+            const error = new Error()
+            errorObj = { code: response.status, message: response.statusText }
+            throw error;
+        }
+
         const result = await response.json();
         return result.alpha2countryCode
     } catch (error) {
-        console.error(error);
+        errorObj.error = error
+        console.error('Error fetching airport country code', errorObj);
         return null
     }
-
 }
 
-const getCountryName = async (IATA: string)=> {
+const getCountryName = async (IATA: string) => {
     const key = process.env.IATA_API_KEY
     const ISO = await getAirportCountryCode(IATA)
 
-    if(!ISO){
+    if (!ISO) {
         return null
     }
 
@@ -40,14 +47,24 @@ const getCountryName = async (IATA: string)=> {
             'X-RapidAPI-Host': 'aviation-reference-data.p.rapidapi.com'
         }
     };
-    
+
+    // Initialize error object
+    let errorObj: { error?: {}, code?: number, message?: string, data?: {} } = {};
+
     try {
         const response = await fetch(url, options);
+        if (!response.ok) {
+            const error = new Error()
+            errorObj = { code: response.status, message: response.statusText }
+            throw error;
+        }
+
         const result = await response.json();
         console.log(result)
         return result.name
     } catch (error) {
-        console.error(error);
+        errorObj.error = error
+        console.error('Error fetching country name', errorObj);
         return null
     }
 }
