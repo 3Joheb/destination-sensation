5eb0d958676d0cfcbaf48b5a72f3dbefe60a29a9
zoheb598 <x22447782@student.ncirl.ie>
2024-04-11 13:27:34 +0100

    [refac] modify db script to point to cloud

--- database/import_to_firestore.js
+++ database/import_to_firestore.js
@@ -1,17 +1,18 @@
 import admin from 'firebase-admin'
 import fs from 'fs'
 import csv from 'csv-parser'
+import data from '../ignore/destination-sensation-beb16-firebase-adminsdk-6lj8m-19b4c6f659.json';
 
 // Initialize Firebase App
 admin.initializeApp({
-    projectId: 'destination-sensation-beb16',
+    credential: admin.credential.cert(data)
 });
 
-// Get a reference to Firestore 
+// Get a reference to Firestore
 const db = admin.firestore();
 
 // Configure database connection for Firestore emulator
-db.settings({ host: '127.0.0.1:8080', ssl: false });
+// db.settings({ host: '127.0.0.1:8080', ssl: false });
 
 const csvFilePath = './docs/modified_airports.csv';
 
