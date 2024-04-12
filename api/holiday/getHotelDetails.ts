b70eeee4ddcab497710cad936d997549f320f14c
zoheb598 <x22447782@student.ncirl.ie>
2024-04-12 16:05:10 +0100

    [chore] move hotel endpoints to holiday endpoint

---
+++ api/holiday/getHotelDetails.ts
@@ -0,0 +1,31 @@
+
+import { VercelRequest, VercelResponse } from '@vercel/node';
+import getAuthToken from '../hotelSearch/getAuthToken.js';
+import { AmadeusOAuth2Token } from '../helpers/getAuthToken.js';
+
+const baseUrl = 'https://test.api.amadeus.com/v3/shopping/hotel-offers';
+
+async (req: VercelRequest, res: VercelResponse) => {
+    const auth = await getAuthToken();
+    if ("error" in auth) {
+        res.json("error");
+    } else {
+        // Extract inputted parameters from the request
+        const hotelIds = req.query.hotelids || '';
+        const adults = req.query.adults || '1'; // Provide a default value if not provided
+
+        // Build the URL with inputted parameters
+        const url = `${baseUrl}?hotelIds=${hotelIds}&adults=${adults}`;
+
+        const options: RequestInit = {
+            headers: {
+                'Authorization': `${auth['token_type']} ${auth['access_token']}`
+            }
+        };
+
+        const hotelReq = await fetch(url, options);
+        const hotelData = await hotelReq.json();
+
+        res.json({ hotels: hotelData });
+    }
+};
