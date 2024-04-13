f1074e43fed022221363e13d243a3a2ef7a14f22
zoheb598 <x22447782@student.ncirl.ie>
2024-04-13 17:25:14 +0100

    Options: [feat] dynamically create cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import "./index.css"
 import Navbar from "./components/Navbar"
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Input from "./pages/Input"
+import Input from "./pages/Holiday/Input"
 import LoginSignup from "./pages/LoginSignup"
 import Home from "./pages/Home"
 import Options from "./pages/Holiday/Options";
--- src/pages/Holiday/Options.tsx
+++ src/pages/Holiday/Options.tsx
@@ -1,33 +1,50 @@
 import { useEffect, useState } from "react"
+import Card from "../../components/Card"
+import fakeData from "./fakeData"
+
 
 const Options = () => {
-    const [options, setOptions] = useState([])
+    const [options, setOptions] = useState(fakeData)
 
-    useEffect(() => {
-        const fetchData = async () => {
-            // Clients current url
-            const url = new URL(window.location.href)
-            const searchParm = url.searchParams
+    // useEffect(() => {
+    //     const fetchData = async () => {
+    //         // Clients current url
+    //         const url = new URL(window.location.href)
+    //         const searchParm = url.searchParams
 
-            try {
-                const optionsRes = await fetch(`/api/flight-inspiration?${searchParm}`)
-                const optionsResult = await optionsRes.json()
-                if (!optionsRes.ok) {
-                    throw new Error('Failed to fetch data') // Maybe show GUI feedback
-                }
+    //         // Send request to backend
+    //         try {
+    //             const optionsRes = await fetch(`/api/flight-inspiration?${searchParm}`)
+    //             const optionsResult = await optionsRes.json()
+    //             if (!optionsRes.ok) {
+    //                 throw new Error('Failed to fetch data') // Maybe show GUI feedback
+    //             }
 
-                setOptions(optionsResult)
-            } catch (error) {
-                console.error('Error fetching data:', error);
-            }
-        }
+    //             setOptions(optionsResult)
+    //             console.log(optionsResult)
+    //         } catch (error) {
+    //             console.error('Error fetching data:', error);
+    //         }
+    //     }
 
-        fetchData()
-    }, [])
+    //     fetchData()
+    // }, [])
 
     return (
         <div>
-            <p></p>
+            <span>Pick one the holidays below</span>
+            <h1>You're one step away from one of these amazing holidays!</h1>
+            <div>
+                {options.map(({ destination, price, departureDate, returnDate, image }, index) => (
+                    <Card
+                        destination={destination.city}
+                        price={price}
+                        departuredate={departureDate}
+                        returndate={returnDate}
+                        key={index}
+                    />
+                ))}
+            </div>
         </div>
     )
 }
