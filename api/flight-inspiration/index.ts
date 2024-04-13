5ac779bfcd970e4a89ad50c1b6c950571f2135e1
zoheb598 <x22447782@student.ncirl.ie>
2024-04-13 16:53:49 +0100

    [refac] use camelCase for response

--- api/flight-inspiration/index.ts
+++ api/flight-inspiration/index.ts
@@ -60,7 +60,7 @@ export default async (req: VercelRequest, res: VercelResponse) => {
                 holiday.destination.country = airport.country
                 holiday.destination.city = airport.city
                 holiday.destination.subdivision = airport.subd
-                holiday.image.hot_link = image.imageHotlink
+                holiday.image.link = image.imageHotlink
                 holiday.image.redirect = image.redirectLink
 
                 holiday.image.tags = image.tags // Remove in prod
