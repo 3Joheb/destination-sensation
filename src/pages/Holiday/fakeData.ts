a98746e612ce374aa3a1d105ccc067a07c6dc38b
zoheb598 <x22447782@student.ncirl.ie>
2024-04-13 18:02:14 +0100

    [chore] change .ignore

---
+++ src/pages/Holiday/fakeData.ts
@@ -0,0 +1,51 @@
+// Use this to prevent hitting api quotas
+export default [
+    {
+        "iata": "RAK",
+        "price": "64.47",
+        "departureDate": "2024-05-05",
+        "returnDate": "2024-05-17",
+        "destination": {
+            "country": "Morocco",
+            "city": "Marrakech",
+            "subdivision": "Marrakesh-Safi"
+        },
+        "image": {
+            "link": "https://pixabay.com/get/gdbe0e97767920b6f496c9cc98f4817b35c52edfca55fc70f4bd94ab5bd592f4d9be4a8a52638a731a468d8e7bd9220e6da5880d37715fd03609b84b3a566e13b_640.jpg",
+            "redirect": "https://pixabay.com/photos/morocco-city-historic-village-clay-2349647/",
+            "tags": "morocco, city, historic"
+        }
+    },
+    {
+        "iata": "LPA",
+        "price": "66.89",
+        "departureDate": "2024-05-05",
+        "returnDate": "2024-05-07",
+        "destination": {
+            "country": "Spain",
+            "city": "Gran Canaria Island",
+            "subdivision": "Canary-Islands"
+        },
+        "image": {
+            "link": "https://pixabay.com/get/g68feabd008b4b53152f51fa80334965032d9bed58740f3a64275b3e81adb53b75946f321e3e2baf55453a3e70a4f51b7dce0548546f9b43e259f60faf60fb95a_640.jpg",
+            "redirect": "https://pixabay.com/photos/sea-shore-waves-storm-darkness-8581570/",
+            "tags": "sea, shore, waves"
+        }
+    },
+    {
+        "iata": "OPO",
+        "price": "71.00",
+        "departureDate": "2024-05-05",
+        "returnDate": "2024-05-11",
+        "destination": {
+            "country": "Portugal",
+            "city": "Porto",
+            "subdivision": "Porto"
+        },
+        "image": {
+            "link": "https://pixabay.com/get/gc1cdcc23635279d1eaaba4798a12449e19e31617ef8bb73612dca27574011f1fcac854c10532eefbff2e6c0abfc899bfd3d76cbd71fe29967b77f8a6a46cefc3_640.jpg",
+            "redirect": "https://pixabay.com/photos/lisbon-tram-portugal-8268841/",
+            "tags": "lisbon, tram, portugal"
+        }
+    }
+]
