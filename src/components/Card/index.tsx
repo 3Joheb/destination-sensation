980a5f0abe69c1444f78da2cefc0d3dfe7da6fcc
zoheb598 <x22447782@student.ncirl.ie>
2024-04-14 13:36:37 +0100

    Card: [feat] add pixabay redirect link

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -22,8 +22,11 @@ interface Cardinterface {
 const Card = ({ destination, price, departureDate, returnDate, image }: Cardinterface) => {
     return (
         <div>
-            <div className='relative w-52 h-52'>
-                <img src={image.link} alt={`Image of ${destination}`} />
+            <div className='relative w-64 h-64'>
+                <img src={image.link} alt={`Image of ${destination}`} className='w-full h-full object-cover' />
+                <a href={image.redirect} target="_blank" rel="noopener noreferrer">
+                    <img src={PixabayLogo} alt='Pixabay Logo' className='absolute bottom-0 right-0 w-24' />
+                </a>
             </div>
             <h1>{destination}</h1>
             <h1>{`€${price}`}</h1>
