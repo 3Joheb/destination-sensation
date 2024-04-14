755adb032d31ee40b7167d1660fefed222dd72e2
zoheb598 <x22447782@student.ncirl.ie>
2024-04-14 14:09:55 +0100

    Card: [style] add overlay to highlight logo

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -15,25 +15,28 @@ interface Cardinterface {
 - Layout and styling
     - Image
     - Ul
-- Pixabay redirect
+- Make pixabay logo more clear
 
 */
 
 const Card = ({ destination, price, departureDate, returnDate, image }: Cardinterface) => {
     return (
-        <div>
+        <div className='flex gap-8'>
             <div className='relative w-64 h-64'>
-                <img src={image.link} alt={`Image of ${destination}`} className='w-full h-full object-cover' />
+                <img src={image.link} alt={`Image of ${destination}`} className='w-64 h-64 object-cover relative' />
+                <div className='absolute top-0 left-0 w-full h-full bg-gradient-to-br from-transparent via-transparent to-black' />
                 <a href={image.redirect} target="_blank" rel="noopener noreferrer">
                     <img src={PixabayLogo} alt='Pixabay Logo' className='absolute bottom-0 right-0 w-24' />
                 </a>
             </div>
-            <h1>{destination}</h1>
-            <h1>{`€${price}`}</h1>
-            <ul>
-                <li>{departureDate}</li>
-                <li>{returnDate}</li>
-            </ul>
+            <div>
+                <h1>{destination}</h1>
+                <ul className='flex gap-4'>
+                    <li>From: {departureDate}</li>
+                    <li>To: {returnDate}</li>
+                </ul>
+                <span>{`€${price}`}</span>
+            </div>
         </div>
     )
 }
