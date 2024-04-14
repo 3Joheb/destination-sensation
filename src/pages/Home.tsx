f352ee1bdf38c9895c3b43fb2cb531c7eed0e892
zoheb598 <x22447782@student.ncirl.ie>
2024-04-14 22:53:14 +0100

    Home: [style] touch up

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,20 +1,29 @@
-import beachImg from "../assets/beach.jpg"
-import cabinImg from "../assets/cabin.jpg"
-import winterImg from "../assets/winter.jpg"
+import porto from "../assets/porto.jpg"
+import rome from "../assets/rome.jpg"
+import venice from "../assets/venice.jpg"
 
 export default () => {
-    return (
-        <div>
-            <div>WELCOME</div>
 
-            <p> Hi there us fellow members of DESTINATION SENSATION are bringing this new holiday package website to you! We aim to help
+    return (
+        <div className="flex flex-col justify-center items-center h-svh gap-14">
+            <span className="text-3xl">Welcome!</span>
+            <p className="max-w-2xl text-xl text-center"> Hi there us fellow members of <span className="font-medium">Destination Sensation</span> are bringing this new holiday package website to you! We aim to help
                 making your holiday descision easier by making the biggest factor in your holiday way easier to chose , you
                 give us a price and time we will find you the places best suited to your budget!
             </p>
-            <div className="images">
-                <img src={beachImg} alt="" />
-                <img src={cabinImg} alt="" />
-                <img src={winterImg} alt="" />
+            <div className="home-imgs">
+                <div className="img-wrap">
+                    <img src={porto} className="w-72" />
+                    <span>Porto</span>
+                </div>
+                <div className="img-wrap">
+                    <img src={rome} className="w-72" />
+                    <span>Rome</span>
+                </div>
+                <div className="img-wrap">
+                    <img src={venice} className="w-72" />
+                    <span>Venice</span>
+                </div>
             </div>
         </div>
     )
