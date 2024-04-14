e9ce9b6cf2f67e0af1c2c2acd2263e9ac1eb0b60
kaitlinmoore21 <117903693+kaitlinmoore21@users.noreply.github.com>
2024-04-14 15:01:33 +0100

    background pic

--- src/components/Payment/Payment.tsx
+++ src/components/Payment/Payment.tsx
@@ -1,7 +1,5 @@
+import '../../index.css'
 
-
-import name_icon from '../../assets/name.jpg'
-import password_icon from '../../assets/password.jpg'
 
 const Payment = () => {
 
@@ -9,23 +7,19 @@ const Payment = () => {
         <div className = 'container'>
         <div className = "header">
         <div className="text">Payment</div>
-        <div className="underline"></div>    
                 </div>
+                
                 <div className="inputs">
          <div className="input">
-        <img src={name_icon} alt="" />
         <input type="Full name" placeholder='Full name'/>
         </div>
          <div className="input">
-        <img src={password_icon} alt="" />
         <input type="Card number" placeholder='Card number'/>
         </div>
          <div className="input">
-        <img src={password_icon} alt="" />
         <input type="Expiry date" placeholder='Expiry Date' />
         </div>
         <div className="input">
-        <img src={password_icon} alt="" />
         <input type="Security number" placeholder='Security Number' />
         </div>
         <div className="submit-container">
@@ -34,7 +28,7 @@ const Payment = () => {
    </div>
    </div>
    
-    )
+    );
 }
 
 export default Payment
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import Payment from './components/Payment/Payment'
-
+import LoginSignup from './components/LoginSignup/LoginSignup.1'
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
-          <Payment/>
+    <Payment/>
+    <LoginSignup/>
     </React.StrictMode>
 )
