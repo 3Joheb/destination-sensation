9b13ab1e758abb4e26d501398333e7fb07305a2f
zoheb598 <x22447782@student.ncirl.ie>
2024-04-14 19:33:48 +0100

    Details: [style] ability to scroll through activities

--- src/pages/Holiday/Details.tsx
+++ src/pages/Holiday/Details.tsx
@@ -4,7 +4,7 @@ import { IoIosArrowRoundDown } from "react-icons/io";
 
 const HolidayDetails = () => {
     const [options, setOptions] = useState(detailsData)
-    const [activities, setActivites] = useState(detailsData.activities.slice(0, 3))
+    const [activities, setActivites] = useState(detailsData.activities.slice(0, 10))
     const [flight, setFlight] = useState(detailsData.flightDetails)
 
     // useEffect(() => {
@@ -40,9 +40,9 @@ const HolidayDetails = () => {
                         <div className="flex flex-col items-center" key={i}>
                             <div>{route.segments.map((segment: any, j: any) => (
                                 <div className="flex flex-col items-center" key={j}>
-                                    <span>{`${flight.airportsMap[segment.departure.iataCode].country}`}</span>
+                                    <span>{`${flight.airportsMap[segment.departure.iataCode].city}`}</span>
                                     <IoIosArrowRoundDown />
-                                    <span>{`${flight.airportsMap[segment.arrival.iataCode].country}`}</span>
+                                    <span>{`${flight.airportsMap[segment.arrival.iataCode].city}`}</span>
                                 </div>
                             ))}</div>
                             <span>Duration: {`${route.duration}`}</span>
@@ -52,14 +52,16 @@ const HolidayDetails = () => {
             </div>
             <div>
                 <h1>Things to do</h1>
-                <div>
-                    {activities.map((activity, i) => (
-                        <div className="" key={i}>
-                            <img src={activity.pictures[0]} alt="" className="w-52 h-52 object-cover" />
-                            <h2>{activity.name}</h2>
-                            <p>{activity.description.short}</p>
-                        </div>
-                    ))}
+                <div className="overflow-y-auto">
+                    <div className="flex gap-10 min-w-fit">
+                        {activities.map((activity, i) => (
+                            <div className="w-52" key={i}>
+                                <img src={activity.pictures[0]} alt="" className="w-52 h-52 object-cover" />
+                                <h2>{activity.name}</h2>
+                                <p>{activity.description.short}</p>
+                            </div>
+                        ))}
+                    </div>
                 </div>
             </div>
         </div>
