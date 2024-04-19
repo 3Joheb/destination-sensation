4b666c156916963f148f8f84117211a5aae40620
kaitlinmoore21 <117903693+kaitlinmoore21@users.noreply.github.com>
2024-04-19 16:29:58 +0100

    gethotellist.test
    kaitlins testing

---
+++ api/holiday/getHotelList.test.ts
@@ -0,0 +1,35 @@
+import fetchMock from 'jest-fetch-mock';
+
+beforeAll(() => {
+    fetchMock.enableMocks();
+});
+
+beforeEach(() => {
+    fetchMock.resetMocks();
+});
+
+describe('getHotelList', () => {
+    it('fetches and processes hotel data correctly', async () => {
+        fetchMock.mockResponseOnce(JSON.stringify({
+        })
+    );
+});
+            {
+                "data"; [
+                    {
+                        "chainCode": "AC",
+                        "iataCode": "PAR",
+                        "dupeId": 700049063,
+                        "name": "HOTEL RESIDENCE EUROPE",
+                        "hotelId": "ACPAR733",
+                        "geoCode": {
+                            "latitude": 48.87321,
+                            "longitude": 2.32541
+                        },
+                        "address": {
+                            "countryCode": "FR"
+                        },
+                        "lastUpdate": "2023-06-15T09:57:36"
+                    
+                    }]}
+                });
